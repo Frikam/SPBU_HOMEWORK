9d24ff8dce70ea068e49985e6d77c90859b57014
Tetin Ilya <tetin.ilya@gmail.com>
2018-11-06 22:22:12 +0300

    Delete extra spaces and rename from randnumb to randomNumber

--- 3/3.4/3.4/main.cpp
+++ 3/3.4/3.4/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int bullsAndCows(int number[], int randnumb[], int numbOfDigits)
+int bullsAndCows(int number[], int randomNumber[], int numbOfDigits)
 {
     int bulls = 0;
     int cows = 0;
@@ -11,27 +11,23 @@ int bullsAndCows(int number[], int randnumb[], int numbOfDigits)
     {
         for (int j = 0; j < numbOfDigits; j++)
         {
-            if (number[i] == randnumb[j])
+            if (number[i] == randomNumber[j])
             {
-                if (i == j)
-                {
+                if (i == j){
                     bulls++;
                 }
-                else
-                {
+                else{
                     cows++;
                 }
             }
         }
     }
     
-    if (bulls == 4)
-    {
+    if (bulls == 4){
         cout << "Congratulations you guessed the number!!!!!!";
     }
-    else
-    {
-    cout << "Quantity Of Cows :" << cows << endl << "Quantity Of bulls :" << bulls << endl;
+    else{
+        cout << "Quantity Of Cows :" << cows << endl << "Quantity Of bulls :" << bulls << endl;
     }
     
     return bulls;
@@ -55,27 +51,28 @@ void enterNumb(int *number, int numbOfDigits)
     int digits = 0;
     cin >> digits;
     int replace = 0;
+    
     for (int i = 0; i < numbOfDigits; i++)
     {
-        replace = (powerOfNumb(10,(numbOfDigits - i - 1)));
+        replace = (powerOfNumb(10, (numbOfDigits - i - 1)));
         number[i] = digits / replace;
         digits = digits % replace;
     }
 }
 
-bool isDifferent(int *randnumber, int numbOfDigits, int number)
+bool isDifferent(int *randomNumber, int numbOfDigits, int number)
 {
     int count = 0;
     for (int i = 0; i < numbOfDigits; i++)
     {
-        if (number == randnumber[i]){
+        if (number == randomNumber[i]){
             count++;
         }
     }
     return (count == 1);
 }
 
-void randomNumber(int *randnumber, int numbOfDigits)
+void makeRandomNumber(int *randomNumber, int numbOfDigits)
 {
     int count = 0;
     while (count != numbOfDigits)
@@ -83,12 +80,12 @@ void randomNumber(int *randnumber, int numbOfDigits)
         count = 0;
         for (int i = 0; i < numbOfDigits; i++)
         {
-            randnumber[i] = (rand() % 10);
+            randomNumber[i] = (rand() % 10);
         }
         
         for (int i = 0; i < numbOfDigits; i++)
         {
-            if (isDifferent(randnumber, numbOfDigits, randnumber[i])){
+            if (isDifferent(randomNumber, numbOfDigits, randomNumber[i])){
                 count++;
             }
         }
@@ -100,18 +97,17 @@ int main()
     srand(time(0));
     cout << "Computer made a number, try to guess it" << endl;
     const int numbOfDigits = 4;
-
-    int randnumber[numbOfDigits];
-    randomNumber(randnumber, numbOfDigits);
-    cout << randnumber[0] << randnumber[1] << randnumber[2] << randnumber[3] << endl;
+    int randomNumber[numbOfDigits];
+    makeRandomNumber(randomNumber, numbOfDigits);
+    cout << randomNumber[0] << randomNumber[1] << randomNumber[2] << randomNumber[3] << endl;
     int number[numbOfDigits];
     int bulls = 0;
     
-    while(bulls != 4)
+    while (bulls != 4)
     {
         cout << "Enter your number : ";
         enterNumb(number, numbOfDigits);
-        bulls = bullsAndCows(number, randnumber, numbOfDigits);
+        bulls = bullsAndCows(number, randomNumber, numbOfDigits);
     }
     return 0;
 }
