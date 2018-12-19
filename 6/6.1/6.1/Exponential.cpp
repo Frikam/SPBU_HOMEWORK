c8e7c17fd34a586a154c454963d3b83d63e04c38
Tetin Ilya <tetin.ilya@gmail.com>
2018-12-19 19:34:02 +0300

    Теперь вывод больше знаков после запятой

--- 6/6.1/6.1/Exponential.cpp
+++ 6/6.1/6.1/Exponential.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
+#include <iomanip>
+
 #include "Exponential.hpp"
 
 using namespace std;
 
 void printSign(double number)
 {
-    int firstNumber = 0b10000000;
+    long long firstNumber = 0b10000000;
     char *binForm = (char*)&number;
     
     if (binForm[7] & firstNumber)
@@ -20,20 +22,20 @@ void printSign(double number)
 
 void printMantissa(double number)
 {
-    int firstNumber = 0b00111111;
-    int secondNumber = 0b11110000;
+    long long firstNumber = 0b00111111;
+    long long secondNumber = 0b11110000;
 
     ((char*)&number)[7] = firstNumber;
     ((char*)&number)[6] = ((char*)&number)[6] | secondNumber;
     
-    cout << number;
+    cout << setprecision(15) << number;
 }
 
 void printExponential(double number)
 {
-    int firstNumber = 0b01111111;
-    int secondNumber = 0b11110000;
-    int thirdNumber = 0b1111111111;
+    long long firstNumber = 0b01111111;
+    long long secondNumber = 0b11110000;
+    long long thirdNumber = 0b1111111111;
     
     char *binForm = (char*)&number;
     
