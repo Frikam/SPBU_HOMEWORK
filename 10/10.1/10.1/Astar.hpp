3ceb06d65783c0b9ce229f24826744c271e5a1bb
Tetin Ilya <tetin.ilya@gmail.com>
2018-12-21 17:19:57 +0300

    Убрал лишнии функции

--- 10/10.1/10.1/Astar.hpp
+++ 10/10.1/10.1/Astar.hpp
@@ -12,9 +12,4 @@ struct Coordinate
     int y;
 };
 
-int h(int x1, int y1, int x2, int y2);
 void aStar(Map *map, bool **isUsed, int currentX, int currentY, Coordinate **rightWay);
-void checkNewPoint(Map *map, Queue *queue, bool **isUsed, int x, int y);
-void addNeighbors(Map *map, Queue *queue, bool **isUsed, int currentX, int currentY);
-void findRightPoint(Map *map, Queue *queue, int &currentX, int &currentY, Coordinate **rightWay);
-void checkNewPoint(Map *map, Queue *queue, bool **isUsed, int x, int y, int x1, int y1);
