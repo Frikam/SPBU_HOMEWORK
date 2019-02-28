d86b16c7f53043285bb0349cf48e735084a46eeb
Tetin Ilya <tetin.ilya@gmail.com>
2019-02-28 13:33:49 +0300

    added task 1

---
+++ sem2/2/1/src/main/java/group144/tetin/InsertionSorter.java
@@ -0,0 +1,26 @@
+package group144.tetin;
+
+public class InsertionSorter implements Sorter{
+    @Override
+    public void sort(int[] array) {
+        int length = array.length;
+        if (length == 0){
+            return;
+        }
+        for (int i = 1; i < length; i++)
+        {
+            int j = i;
+            while (j != 0 && array[j] < array[j - 1]){
+                swap(j, j-1, array);
+                j--;
+            }
+        }
+    }
+
+
+    private void swap(int a, int b, int[] array){
+        int tmp = array[a];
+        array[a] = array[b];
+        array[b] = tmp;
+    }
+}
---
+++ sem2/2/1/src/test/java/group144/tetin/BogoSorterTest.java
@@ -0,0 +1,44 @@
+package group144.tetin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BogoSorterTest {
+    @Test
+    public void sortReversed(){
+        int[] array = {5, 4, 3, 2, 1};
+        int[] sortedArray = {1, 2, 3, 4, 5};
+        Sorter sorter = new BogoSorter();
+        sorter.sort(array);
+        assertArrayEquals(sortedArray, array);
+    }
+
+    @Test
+    public void sortSorted(){
+        int[] array = {1, 2, 3, 4, 5};
+        int[] sortedArray = {1, 2, 3, 4, 5};
+        Sorter sorter = new BogoSorter();
+        sorter.sort(array);
+        assertArrayEquals(sortedArray, array);
+    }
+
+    @Test
+    public void sortRandomly(){
+        int[] array = {4, 5, 3, 1, 2};
+        int[] sortedArray = {1, 2, 3, 4, 5};
+        Sorter sorter = new BogoSorter();
+        sorter.sort(array);
+        assertArrayEquals(sortedArray, array);
+    }
+
+    @Test
+    public void sortEmpty(){
+        int[] array = {};
+        int[] sortedArray = {};
+        Sorter sorter = new BogoSorter();
+        sorter.sort(array);
+        assertArrayEquals(sortedArray, array);
+    }
+
+}
