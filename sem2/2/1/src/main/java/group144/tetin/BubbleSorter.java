7d9b6ae29e7b21aed49b50e0c4ec3305ed1fb574
Tetin Ilya <tetin.ilya@gmail.com>
2019-02-28 18:21:16 +0300

    added task 1

---
+++ sem2/2/1/src/main/java/group144/tetin/BubbleSorter.java
@@ -0,0 +1,25 @@
+package group144.tetin;
+
+public class BubbleSorter implements Sorter {
+
+    @Override
+    public void sort(int[] array) {
+        int length = array.length;
+        if (length == 0){
+            return;
+        }
+        for (int i = 0; i < length - 1; i++){
+            for (int j = 0; j < length - i - 1; j++){
+                if (array[j] > array[j + 1]){
+                    swap(j,j + 1, array);
+                }
+            }
+        }
+    }
+
+    private void swap(int a, int b, int[] array){
+        int tmp = array[a];
+        array[a] = array[b];
+        array[b] = tmp;
+    }
+}
---
+++ sem2/2/1/src/main/java/group144/tetin/Sorter.java
@@ -0,0 +1,6 @@
+package group144.tetin;
+
+
+public interface Sorter {
+    public void sort(int[] array);
+}
