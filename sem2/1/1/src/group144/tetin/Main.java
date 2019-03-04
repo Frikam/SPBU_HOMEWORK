2383ff646e739b3018a20bc2bb08174ec387332b
Tetin Ilya <tetin.ilya@gmail.com>
2019-03-04 17:52:01 +0300

    delete extra spaces, rename file from main to Main

---
+++ sem2/1/1/src/group144/tetin/Main.java
@@ -0,0 +1,15 @@
+package group144.tetin;
+
+public class Main {
+    public static void main(String args[]){
+        Stack stack = new Stack();
+        System.out.println("Hello");
+        stack.push(5);
+        stack.push(10);
+        System.out.println("Size : " + stack.getSize());
+        System.out.println("Deleted element : " + stack.pop());
+        System.out.println("Size : " + stack.getSize());
+        System.out.println("Deleted element : " + stack.pop());
+        System.out.println("Stack is empty : " + stack.isEmpty());
+    }
+}
---
+++ sem2/1/1/src/group144/tetin/Stack.java
@@ -0,0 +1,46 @@
+package group144.tetin;
+
+import java.util.EmptyStackException;
+
+public class Stack {
+
+    private StackElement head = null;
+    private int size = 0;
+
+    /**Add element in head*/
+    public void push(int value){
+        head = new StackElement(value, head);
+        size += 1;
+    }
+
+    /**Delete element from head*/
+    public int pop() {
+        if (size == 0){
+            System.out.println("Stack is empty");
+            return 0;
+        }
+
+        int result = head.value;
+        head = head.next;
+        size-=1;
+        return result;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    private class StackElement{
+        int value;
+        StackElement next;
+
+        StackElement(int value, StackElement next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
