0f2697fd17fc149894a8d3325da03e7a9bf86aa0
Tetin Ilya <tetin.ilya@gmail.com>
2019-05-05 20:41:30 +0300

    make  word.charAt(i)-'a' variable and replace name class from Bor to Trie

--- sem2/7/1/src/main/java/group144/tetin/Trie.java
+++ sem2/7/1/src/main/java/group144/tetin/Trie.java
@@ -1,14 +1,17 @@
 package group144.tetin;
 
 import java.io.*;
+import java.util.ArrayList;
 
-/** A class that represent bor data structure */
-public class Bor implements Serializable {
+/** A class that represent trie data structure */
+public class Trie implements Serializable {
     private final int SIZE_OF_ALPHABET = 26;
     private Node head = new Node();
     private int size = 0;
+    private ArrayList<String> words = new ArrayList<>();
 
-    /** A method that adds element in bor */
+    /** A method that adds element in trie
+     * @throws WrongSymbolException when word have symbol not from alphabetical */
     public boolean add(String word) throws WrongSymbolException {
         boolean isContains = contains(word);
 
@@ -21,53 +24,59 @@ public class Bor implements Serializable {
 
         Node current = head;
         int size = word.length();
+        int index;
 
         for (int i = 0; i < size; i++) {
+            index = word.charAt(i) - 'a';
             if (isWrongSymbol(word.charAt(i))) {
                 throw new WrongSymbolException();
             }
-            if (current.next[word.charAt(i) - 'a'] == null) {
-                current.next[word.charAt(i) - 'a'] = new Node();
+            if (current.next[index] == null) {
+                current.next[index] = new Node();
             }
 
-            current.next[word.charAt(i) - 'a'].numberOfPrefix++;
+            current.next[index].numberOfPrefix++;
 
             if (i == word.length() - 1) {
-                current.next[word.charAt(i) - 'a'].isTerminal = true;
+                current.next[index].isTerminal = true;
+                words.add(word);
             }
             else {
-                current = current.next[word.charAt(i) - 'a'];
+                current = current.next[index];
             }
         }
 
         return isContains;
     }
 
-    /** A method that removes elements from bor */
+
+    /** A method that removes elements from trie */
     public boolean remove(String word) throws WrongSymbolException {
         boolean isContains = contains(word);
 
-        if (isContains) {
-            size--;
+        if (!isContains) {
+            return false;
         }
         else {
-            return false;
+            size--;
         }
 
         int size = word.length();
         Node current = head;
         Node previous;
+        int index;
 
         for (int i = 0; i < size; i++) {
+            index = word.charAt(i) - 'a';
             if (isWrongSymbol(word.charAt(i))) {
                 throw new WrongSymbolException();
             }
 
-            if (current.next[word.charAt(i) - 'a'] == null) {
+            if (current.next[index] == null) {
                 return false;
             }
             previous = current;
-            current = current.next[word.charAt(i) - 'a'];
+            current = current.next[index];
             current.numberOfPrefix--;
 
             if (current.numberOfPrefix == 0) {
@@ -80,20 +89,22 @@ public class Bor implements Serializable {
         return isContains;
     }
 
-    /** A method that checks element in bor or no */
+    /** A method that checks element in trie or no */
     public boolean contains(String word) throws WrongSymbolException {
         int size = word.length();
         Node current = head;
+        int index;
 
         for (int i = 0; i < size; i++) {
+            index = word.charAt(i) - 'a';
             if (isWrongSymbol(word.charAt(i))) {
                 throw new WrongSymbolException();
             }
 
-            if (current.next[word.charAt(i) - 'a'] == null) {
+            if (current.next[index] == null) {
                 return false;
             }
-            current = current.next[word.charAt(i) - 'a'];
+            current = current.next[index];
         }
 
         if (current.isTerminal == false) {
@@ -123,22 +134,36 @@ public class Bor implements Serializable {
         return current.numberOfPrefix;
     }
 
-    /** A method that return size of bor */
+    /** A method that return size of trie */
     public int size() {
         return size;
     }
 
     /** A method that puts object in output stream to save it */
     public void serialize(OutputStream out) throws IOException {
-        ObjectOutputStream object = new ObjectOutputStream(out);
-        object.writeObject(this);
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+
+        for (int i = 0; i < words.size(); i++) {
+            writer.write(words.get(i));
+            writer.write("\n");
+        }
+
+        writer.close();
     }
 
     /** A method that gets object from input stream and replace current one with it */
-    public void deserialize(InputStream in) throws IOException, ClassNotFoundException {
-        ObjectInputStream object = new ObjectInputStream(in);
-        Bor newTrie = (Bor) object.readObject();
-        this.head = newTrie.head;
+    public void deserialize(InputStream in) throws IOException, WrongSymbolException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(in));
+        Trie trie = new Trie();
+
+        while (input.ready()) {
+            trie.add(input.readLine());
+        }
+
+        head = trie.head;
+        size = trie.size;
+
+        input.close();
     }
 
     private boolean isWrongSymbol(char symbol) {
