af080dd896e05423d9290e0d2a2b5ff53c397c95
Tetin Ilya <tetin.ilya@gmail.com>
2019-05-11 18:25:48 +0300

    Теперь проверка на победу игроков привязанна к размеру доски

--- sem2/6/2/src/main/java/group144/tetin/TicTakToe.java
+++ sem2/6/2/src/main/java/group144/tetin/TicTakToe.java
@@ -5,24 +5,52 @@ import javafx.scene.control.Button;
 
 public class TicTakToe {
     public boolean isAnybodyWin(Button[][] buttons) {
-        for (int i = 0; i < 3; i++) {
-            if (!buttons[i][0].getText().equals("") && buttons[i][0].getText() == buttons[i][1].getText() && buttons[i][1].getText() == buttons[i][2].getText()) {
-                return true;
+        int numberOfButtons = buttons.length;
+        boolean result = true;
+        for (int j = 0; j < numberOfButtons; j++) {
+            result = true;
+            for (int i = 0; i < numberOfButtons - 1; i++) {
+                if (!(!buttons[i][j].getText().equals("") && buttons[i][j].getText().equals(buttons[i + 1][j].getText()))) {
+                    result = false;
+                }
+            }
+
+            if (result == true) {
+                return result;
+            }
+
+            result = true;
+            for (int i = 0; i < numberOfButtons - 1; i++) {
+                if (!(!buttons[j][i].getText().equals("") && buttons[j][i].getText().equals(buttons[j][i + 1].getText()))) {
+                    result = false;
+                }
+            }
+
+            if (result == true) {
+                return result;
             }
         }
 
-        for (int i = 0; i < 3; i++) {
-            if (!buttons[0][i].getText().equals("") && buttons[0][i].getText() == buttons[1][i].getText() && buttons[1][i].getText() == buttons[2][i].getText()) {
-                return true;
+        result = true;
+        for (int i = 0; i < numberOfButtons - 1; i++) {
+            if (!(!buttons[i][i].getText().equals("") && buttons[i][i].getText().equals(buttons[i + 1][i + 1].getText()))) {
+                result = false;
             }
         }
 
-        if (!buttons[0][0].getText().equals("") && buttons[0][0].getText() == buttons[1][1].getText() && buttons[1][1].getText() == buttons[2][2].getText()) {
-            return true;
+        if (result == true) {
+            return result;
         }
 
-        if (!buttons[2][0].getText().equals("") && buttons[2][0].getText() == buttons[1][1].getText() && buttons[1][1].getText() == buttons[0][2].getText()) {
-            return true;
+        result = true;
+        for (int i = numberOfButtons - 1; i > 0; i--) {
+            if (!(!buttons[numberOfButtons - 1 - i][i].getText().equals("") && buttons[numberOfButtons - 1 - i][i].getText().equals(buttons[numberOfButtons - i][i - 1].getText()))) {
+                System.out.print("Text : " + buttons[i][i].getText());
+                result = false;
+            }
+        }
+        if (result == true) {
+            return result;
         }
 
         return false;
