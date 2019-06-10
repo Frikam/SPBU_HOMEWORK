d4f3f7f076d318f3f5e81e2f580ab54caacabfb6
Tetin Ilya <tetin.ilya@gmail.com>
2019-06-10 17:10:40 +0300

    update .gitignore file and now  gradle see wrapper proporties file

---
+++ sem2/2/3/src/main/java/group144/tetin/OutputInFile.java
@@ -0,0 +1,12 @@
+package group144.tetin;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+/** Class represent spiral writer output to the file */
+public class OutputInFile extends SpriralOutputter implements SpiralWriter {
+    @Override
+    public void printSpiral(int[][] matrix) throws FileNotFoundException {
+        spiralPrinter(matrix, new PrintStream("output.txt"));
+    }
+}
---
+++ sem2/2/3/src/main/java/group144/tetin/SpriralOutputter.java
@@ -0,0 +1,44 @@
+package group144.tetin;
+
+import java.io.PrintStream;
+
+/** Class represents spiral writer as string */
+public class SpriralOutputter {
+    public void spiralPrinter(int[][] matrix, PrintStream stream) {
+        int num = matrix.length;
+        int step = 0;
+        int index1 = num / 2;
+        int index2 = num / 2;
+        stream.print(matrix[index1][index2] + " ");
+
+        while (index1 != 0 || index2 != num - 1) {
+            step++;
+            for (int j = 0; j < step; j++) {
+                index2--;
+                stream.print(matrix[index1][index2] + " ");
+            }
+
+            for (int j = 0; j < step; j++) {
+                index1++;
+                stream.print(matrix[index1][index2] + " ");
+            }
+
+            step++;
+            for (int j = 0; j < step; j++) {
+                index2++;
+                stream.print(matrix[index1][index2] + " ");
+            }
+
+            for (int j = 0; j < step; j++) {
+                index1--;
+                stream.print(matrix[index1][index2] + " ");
+            }
+        }
+
+        for (int j = 0; j < step; j++) {
+            index2--;
+            stream.print(matrix[index1][index2] + " ");
+        }
+    }
+
+}
