bd4d118e2df015e32fc0872dcec2ee2c9d976748
Tetin Ilya <tetin.ilya@gmail.com>
2019-06-11 12:05:28 +0300

    update java docs

--- sem2/8/2/src/main/java/group144/tetin/LazyFactor.java
+++ sem2/8/2/src/main/java/group144/tetin/LazyFactor.java
@@ -5,12 +5,15 @@ import java.util.function.Supplier;
 public class LazyFactor {
 
     /**
-     * A method that represent one thread lazy calculation
+     * A method that represent create thread lazy calculation
      */
     public static <T> Lazy<T> createOneThreadLazy(Supplier<T> supplier) {
         return createOneThread(supplier);
     }
 
+    /**
+     * A method that represent one thread lazy calculation
+     */
     private static <T> Lazy<T> createOneThread(Supplier<T> supplier) {
         Lazy<T> thread = new Lazy<T>() {
             private boolean wasCalculated = false;
@@ -30,12 +33,15 @@ public class LazyFactor {
     }
 
     /**
-     * A method that represent multi threads lazy calculation
+     * A method that create multi threads lazy calculation
      */
     public static <T> Lazy<T> createMultiThreadLazy(Supplier<T> supplier) {
         return createMultiThread(supplier);
     }
 
+    /**
+     * A method that represent multi threads lazy calculation
+     */
     private static <T> Lazy<T> createMultiThread(Supplier<T> supplier) {
         Lazy<T> thread = new Lazy<T>() {
             private volatile boolean wasCalculated = false;
