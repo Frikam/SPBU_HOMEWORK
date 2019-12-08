4b5e9cc7c50fb90192edef5ceba2bae21930777a
Tetin Ilya <tetin.ilya@gmail.com>
2019-12-08 22:50:56 +0300

    change max angle of rotation of cannon

--- sem3/3/1/src/main/java/Config.java
+++ sem3/3/1/src/main/java/Config.java
@@ -1,7 +1,7 @@
 public class Config {
     public static final int WIDTH_OF_CANNON = 40;
     public static final int START_X_COORDINATE_OF_CANNON = 451;
-    public static final int MAX_ANGLE = -37;
+    public static final int MAX_ANGLE = -130;
     public static final int MIN_ANGLE = 4;
     public static final int START_ANGLE = 35;
     public static final int Y_COORDINATE_OF_GROUND = 440;
