a7e53702dedb31dc92bca7322c354b72e96a1d6d
Tetin Ilya <tetin.ilya@gmail.com>
2020-03-11 21:44:39 +0300

    delete task 4 from hw 2 from master

--- sem4/2/4/PositionOfNumber.hs
+++
@@ -1,4 +0,0 @@
-positionOfNumber :: Integer -> [Integer] -> Integer
-positionOfNumber number list = findPositionOfNumber number list 0
-
-findPositionOfNumber number (x:xs) index = if number == x then index else findPositionOfNumber number xs (index + 1)
